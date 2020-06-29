a052394d68fbc4d0087c22237cf1c71e8619d11f
james wu <jwu_nyc@yahoo.com>
2020-06-29 02:53:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  }#,
+  # :gender => {
+  #   :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+  #   :female => ["Queenie", "Ms. K"]
+  # },
+  # :lives => {
+  #   "Subway" => ["Theo", "Queenie"],
+  #   "Central Park" => ["Alex", "Ms. K", "Lucky"],
+  #   "Library" => ["Peter Jr."],
+  #   "City Hall" => ["Andrew"]
+  # }
+}
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  first_layer=""
+  second_layer=""
+  third_layer=""
+  hash_starting_names={"none"}
+  data.each do |k,v|
+    # puts k
+    # puts "This is the values #{v}"
+    v.each do |ko,names|
+      # puts ko
+      # puts "This is 2nd layer #{names}"
+        names.each do |nom|
+          puts "this is innner #{nom}"
+          p hash_starting_name
+        end
+    end
+  end
 end
+
+p nyc_pigeon_organizer(pigeon_data)
