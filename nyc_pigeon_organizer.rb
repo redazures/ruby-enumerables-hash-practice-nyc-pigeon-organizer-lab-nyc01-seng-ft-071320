3078a33364e9c8f6c204bdeaa65c71e5b59ef469
james wu <jwu_nyc@yahoo.com>
2020-06-29 02:55:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,17 +4,17 @@ pigeon_data = {
     :grey => ["Theo", "Peter Jr.", "Ms. K"],
     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
     :brown => ["Queenie", "Alex"]
-  }#,
-  # :gender => {
-  #   :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-  #   :female => ["Queenie", "Ms. K"]
-  # },
-  # :lives => {
-  #   "Subway" => ["Theo", "Queenie"],
-  #   "Central Park" => ["Alex", "Ms. K", "Lucky"],
-  #   "Library" => ["Peter Jr."],
-  #   "City Hall" => ["Andrew"]
-  # }
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
 }
 
 def nyc_pigeon_organizer(data)
@@ -22,19 +22,34 @@ def nyc_pigeon_organizer(data)
   first_layer=""
   second_layer=""
   third_layer=""
-  hash_starting_names={"none"}
+  hash_starting_names={}
   data.each do |k,v|
     # puts k
+    first_layer=k
+    # puts first_layer
     # puts "This is the values #{v}"
     v.each do |ko,names|
-      # puts ko
+      puts ko
+      second_layer=
       # puts "This is 2nd layer #{names}"
         names.each do |nom|
-          puts "this is innner #{nom}"
-          p hash_starting_name
+          #nom is names of the pigeons. Here I use nom to create the pigeon
+          # puts ":#{nom}"
+          hash_starting_names[nom]={first_layer=>["placeholder"]} if hash_starting_names[nom]==nil
+          # puts first_layer
+          hash_starting_names[nom][first_layer]=["placeholder"] if hash_starting_names[nom][first_layer]==nil
+          #Here we create the pigeon and the color
+          #color, gender, and lives are the items of the first layer
         end
+        #The prior lupe finished creating the pigeon and the color category
     end
   end
+  #creating hash from scratch from here
+  # hash_starting_names.each do |name,placeholder|
+  #    hash_starting_names[name]={first_layer=>placeholder}
+  #  end
+  #This is the output area
+  p hash_starting_names
 end
 
-p nyc_pigeon_organizer(pigeon_data)
+x =nyc_pigeon_organizer(pigeon_data)
